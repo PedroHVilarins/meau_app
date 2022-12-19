cb83db108c4ed4a4d6a84639859f7e197dd306ac
Pedro Henrique Da Costa Vilarins <pedrovilarins077@gmail.com>
2022-12-19 16:26:20 -0300

    latest commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:firebase_core/firebase_core.dart';
-import 'screens/SplashScreen/index.dart';
-import 'utils/firebase_options.dart';
 import 'package:flutter/material.dart';
+
+import 'utils/firebase_options.dart';
+import 'screens/SplashScreen/index.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
